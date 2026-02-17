bb22af66ab33b41cb89552d2a861a71c5b8849d7
ngacho <brandonngacho@gmail.com>
2026-02-17 12:23:47 -0800

    add goals routers

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -3,6 +3,7 @@
 from app.routers.auth import router as auth_router
 from app.routers.budget_templates import router as budget_templates_router
 from app.routers.categories import router as categories_router
+from app.routers.goals import router as goals_router
 from app.routers.plaid import router as plaid_router
 from app.routers.simplefin import router as simplefin_router
 from app.routers.snapshots import router as snapshots_router
@@ -13,6 +14,7 @@ __all__ = [
     "auth_router",
     "budget_templates_router",
     "categories_router",
+    "goals_router",
     "plaid_router",
     "simplefin_router",
     "snapshots_router",
